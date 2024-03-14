8a262ab704817e015072b68fb462948cf3f99335
Anthony T <atrot1@gmail.com>
2024-03-14 15:23:01 -0600

    Somehow destoryed PBR.cpp with the last git commit.

--- Renderer/src/Renderer/PBR.cpp
+++ Renderer/src/Renderer/PBR.cpp
@@ -3,19 +3,11 @@
 
 void PBR::Load(const char* filepath)
 {
-    equirectangularToCubemapShader.Load("res/shaders/hdrCubemap.vert", "res/shaders/hdrCubemap.frag");
-    irradianceShader.Load("res/shaders/hdrCubemap.vert", "res/shaders/irradiance.frag");
+    hdrToCubemap.Load("res/shaders/hdrCubemap.vert", "res/shaders/hdrCubemap.frag");
+    irradiance.Load("res/shaders/hdrCubemap.vert", "res/shaders/irradiance.frag");
 
-    glGenFramebuffers(1, &captureFBO);
-    glGenRenderbuffers(1, &captureRBO);
-
-    glBindFramebuffer(GL_FRAMEBUFFER, captureFBO);
-    glBindRenderbuffer(GL_RENDERBUFFER, captureRBO);
-    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT24, cubemapSize, cubemapSize);
-    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, captureRBO);
-
-    stbi_set_flip_vertically_on_load(true);
     int width, height, nrComponents;
+    stbi_set_flip_vertically_on_load(true);
     float* data = stbi_loadf(filepath, &width, &height, &nrComponents, 0);
     if (data)
     {
@@ -27,27 +19,22 @@ void PBR::Load(const char* filepath)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glGenerateMipmap(GL_TEXTURE_CUBE_MAP);
+        std::cout << "Loaded HDR texture " << filepath << "\n";
 
         stbi_image_free(data);
-<<<<<<< HEAD
 
         CreateEnvironmentCubemap();
         CreateIrradianceMap();
-=======
->>>>>>> aa0264bb92dbf7adb98928243dc685fb088ad3c3
     }
     else
     {
-        std::cout << "Failed to load HDR image." << std::endl;
+        std::cout << "Failed to load HDR texture " << filepath << "\n";
     }
-
-    CreateEnvironmentCubemap();
-    CreateIrradianceMap();
 }
 
 void PBR::CreateEnvironmentCubemap()
 {
-<<<<<<< HEAD
     glGenFramebuffers(1, &captureFBO);
     glGenRenderbuffers(1, &captureRBO);
 
@@ -59,43 +46,26 @@ void PBR::CreateEnvironmentCubemap()
     glGenTextures(1, &envCubemap);
     glBindTexture(GL_TEXTURE_CUBE_MAP, envCubemap);
     for (uint32_t i = 0; i < 6; ++i)
-=======
-    glGenTextures(1, &envCubemap);
-    glBindTexture(GL_TEXTURE_CUBE_MAP, envCubemap);
-    for (unsigned int i = 0; i < 6; ++i)
->>>>>>> aa0264bb92dbf7adb98928243dc685fb088ad3c3
     {
         glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB16F, cubemapSize, cubemapSize, 0, GL_RGB, GL_FLOAT, nullptr);
     }
+
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-<<<<<<< HEAD
     hdrToCubemap.Bind();
     hdrToCubemap.SetMat4("projection", captureProjection);
     hdrToCubemap.SetInt("hdrTexture", 0);
-=======
-    equirectangularToCubemapShader.Bind();
-    equirectangularToCubemapShader.SetInt("equirectangularMap", 0);
-    equirectangularToCubemapShader.SetMat4("projection", captureProjection);
->>>>>>> aa0264bb92dbf7adb98928243dc685fb088ad3c3
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, hdrTexture);
 
     glViewport(0, 0, cubemapSize, cubemapSize);
-<<<<<<< HEAD
     for (uint32_t i = 0; i < 6; ++i)
     {
         hdrToCubemap.SetMat4("view", captureViews[i]);
-=======
-    glBindFramebuffer(GL_FRAMEBUFFER, captureFBO);
-    for (unsigned int i = 0; i < 6; ++i)
-    {
-        equirectangularToCubemapShader.SetMat4("view", captureViews[i]);
->>>>>>> aa0264bb92dbf7adb98928243dc685fb088ad3c3
         glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, envCubemap, 0);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         Model::DrawCube();
@@ -115,17 +85,17 @@ void PBR::CreateIrradianceMap()
 
     glGenTextures(1, &irradianceMap);
     glBindTexture(GL_TEXTURE_CUBE_MAP, irradianceMap);
-    for (unsigned int i = 0; i < 6; ++i)
+    for (uint32_t i = 0; i < 6; ++i)
     {
         glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB16F, irradianceSize, irradianceSize, 0, GL_RGB, GL_FLOAT, nullptr);
     }
+
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
-<<<<<<< HEAD
     hdrToCubemap.Bind();
     hdrToCubemap.SetMat4("projection", captureProjection);
     hdrToCubemap.SetInt("hdrTexture", 0);
@@ -136,23 +106,6 @@ void PBR::CreateIrradianceMap()
     for (uint32_t i = 0; i < 6; ++i)
     {
         hdrToCubemap.SetMat4("view", captureViews[i]);
-=======
-    glBindFramebuffer(GL_FRAMEBUFFER, captureFBO);
-    glBindRenderbuffer(GL_RENDERBUFFER, captureRBO);
-    glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT24, irradianceSize, irradianceSize);
-
-    irradianceShader.Bind();
-    irradianceShader.SetInt("environmentMap", 0);
-    irradianceShader.SetMat4("projection", captureProjection);
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_CUBE_MAP, envCubemap);
-
-    glViewport(0, 0, irradianceSize, irradianceSize);
-    glBindFramebuffer(GL_FRAMEBUFFER, captureFBO);
-    for (unsigned int i = 0; i < 6; ++i)
-    {
-        irradianceShader.SetMat4("view", captureViews[i]);
->>>>>>> aa0264bb92dbf7adb98928243dc685fb088ad3c3
         glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, irradianceMap, 0);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         Model::DrawCube();
